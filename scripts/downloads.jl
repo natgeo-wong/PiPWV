5ea4c506ea504100323242ca371d0ad2cfdfd2e6
Nathanael Wong <natgeo.wong@outlook.com>
2020-03-10 16:37:21 -0400

    Updated for new erainitialize formatting.

--- scripts/downloads.jl
+++ scripts/downloads.jl
@@ -5,24 +5,24 @@ using ClimateERA
 global_logger(ConsoleLogger(stdout,Logging.Info))
 
 init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(1,3,1,0,init);
+emod,epar,ereg,etime = erainitialize(init,modID=1,parID=3,regID=1,timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
 init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(1,7,1,0,init);
+emod,epar,ereg,etime = erainitialize(init,modID=1,parID=7,regID=1,timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
 init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(2,4,1,0,init);
+emod,epar,ereg,etime = erainitialize(init,modID=2,parID=4,regID=1,timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
 init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(2,5,1,0,init);
+emod,epar,ereg,etime = erainitialize(init,modID=2,parID=5,regID=1,timeID=0);
 
 init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(3.7,1,0,init);
+emod,epar,ereg,etime = erainitialize(init,modID=3,parID=7,regID=1,timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
 init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(4,3,1,0,init);
+emod,epar,ereg,etime = erainitialize(init,modID=4,parID=3,regID=1,timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
