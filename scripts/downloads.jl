8653d90b87d1c674cfc7f8f610da43fd9230af4d
Nathanael Wong <natgeo.wong@outlook.com>
2020-03-12 16:40:46 -0400

    Updated according to ClimateERA.jl new input format.

--- scripts/downloads.jl
+++ scripts/downloads.jl
@@ -4,25 +4,26 @@ using ClimateERA
 
 global_logger(ConsoleLogger(stdout,Logging.Info))
 
-init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID=1,parID=3,regID=1,timeID=0);
+init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
+emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="t_sfc",regID="GLB",timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID=1,parID=7,regID=1,timeID=0);
+init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
+emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="z_sfc",regID="GLB",timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID=2,parID=4,regID=1,timeID=0);
+init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
+emod,epar,ereg,etime = erainitialize(init,modID="dpre",parID="t_air",regID="GLB",timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID=2,parID=5,regID=1,timeID=0);
-
-init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID=3,parID=7,regID=1,timeID=0);
+init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
+emod,epar,ereg,etime = erainitialize(init,modID="dpre",parID="z_air",regID="GLB",timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,"/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID=4,parID=3,regID=1,timeID=0);
+init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
+emod,epar,ereg,etime = erainitialize(init,modID="msfc",parID="t_dew",regID="GLB",timeID=0);
+eradownload(emod,epar,ereg,etime,eroot)
+
+init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
+emod,epar,ereg,etime = erainitialize(init,modID="mpre",parID="shum",regID="GLB",timeID=0);
 eradownload(emod,epar,ereg,etime,eroot)
