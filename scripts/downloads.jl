d9d068bf7fd2996c57eaf9697d208cf349be1fbe
Nathanael Wong <natgeo.wong@outlook.com>
2020-04-11 20:31:23 -0400

    Updating download methods using updated ClimateERA methods

--- scripts/downloads.jl
+++ scripts/downloads.jl
@@ -4,26 +4,25 @@ using ClimateERA
 
 global_logger(ConsoleLogger(stdout,Logging.Info))
 
-init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="t_sfc",regID="GLB",timeID=0);
+init,eroot = erastartup(aID=1,dID=1,path="/n/holyscratch01/kuang_lab/nwong/ecmwf/");
+
+emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="t_sfc");
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="z_sfc",regID="GLB",timeID=0);
+emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="z_sfc");
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID="dpre",parID="t_air",regID="GLB",timeID=0);
+emod,epar,ereg,etime = erainitialize(init,modID="dpre",parID="t_air");
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID="dpre",parID="z_air",regID="GLB",timeID=0);
+emod,epar,ereg,etime = erainitialize(init,modID="dpre",parID="z_air");
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID="msfc",parID="t_dew",regID="GLB",timeID=0);
+emod,epar,ereg,etime = erainitialize(init,modID="msfc",parID="t_dew");
 eradownload(emod,epar,ereg,etime,eroot)
 
-init,eroot = erastartup(1,1,path="/n/holyscratch01/kuang_lab/lab/ecmwf/");
-emod,epar,ereg,etime = erainitialize(init,modID="mpre",parID="shum",regID="GLB",timeID=0);
+emod,epar,ereg,etime = erainitialize(init,modID="mpre",parID="shum");
+eradownload(emod,epar,ereg,etime,eroot)
+
+emod,epar,ereg,etime = erainitialize(init,modID="dsfc",parID="p_sfc");
 eradownload(emod,epar,ereg,etime,eroot)
