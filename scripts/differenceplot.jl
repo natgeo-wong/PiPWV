133569171cd0e5aec934e712b58b64908d0c32d2
Nathanael Wong <natgeo.wong@outlook.com>
2020-06-25 04:51:19 -0400

    More plotting script updates!

--- scripts/differenceplot.jl
+++ scripts/differenceplot.jl
@@ -144,11 +144,144 @@ function plotaxs2(ID::AbstractString,cID::AbstractString="RE5")
 
 end
 
-plotaxs("REP");
-plotaxs("REI");
-plotaxs("RGA");; plotaxs("RGA","REI");
-plotaxs("EBB");
-plotaxs("EBM");
+function plotaxscombined(ID1::AbstractString,ID2::AbstractString,cID::AbstractString="RE5")
 
-plotaxs2("EG2"); plotaxs2("EG2","REI");
-plotaxs2("EMN");
+    @load datadir("compiled/Pi_$cID.jld2") lon lat Pi_avg Pi_dhr Pi_sea Pi_ian Pi_itr
+    cID_avg = Pi_avg; cID_dhr = Pi_dhr; cID_sea = Pi_sea;
+    cID_ian = Pi_ian; cID_itr = Pi_itr;
+    @load datadir("compiled/Pi_$ID1.jld2") lon lat Pi_avg Pi_dhr Pi_sea Pi_ian Pi_itr
+    ID1_avg = Pi_avg; ID1_dhr = Pi_dhr; ID1_sea = Pi_sea;
+    ID1_ian = Pi_ian; ID1_itr = Pi_itr;
+    @load datadir("compiled/Pi_$ID2.jld2") lon lat Pi_avg Pi_dhr Pi_sea Pi_ian Pi_itr
+    ID2_avg = Pi_avg; ID2_dhr = Pi_dhr; ID2_sea = Pi_sea;
+    ID2_ian = Pi_ian; ID2_itr = Pi_itr;
+
+    arr = [[1,1,6,6],[2,2,7,7],[3,3,8,8],[4,4,9,9],[5,5,10,10]]
+    pplt.close(); proj = pplt.Proj("robin",lon_0=180); f,axs = pplt.subplots(
+        arr,axwidth=3,proj=proj
+    )
+
+    normPi!(ID1_avg,ID1_dhr); normPi!(ID1_avg,ID1_sea); normPi!(ID1_avg,ID1_ian);
+    normPi!(ID1_avg,ID1_itr);
+    normPi!(ID2_avg,ID2_dhr); normPi!(ID2_avg,ID2_sea); normPi!(ID2_avg,ID2_ian);
+    normPi!(ID2_avg,ID2_itr);
+    normPi!(cID_avg,cID_dhr); normPi!(cID_avg,cID_sea); normPi!(cID_avg,cID_ian);
+    normPi!(cID_avg,cID_itr);
+
+    c = axs[1].contourf(
+        lon,lat,((ID1_avg.-cID_avg)./cID_avg)'*100,
+        cmap="RdBu_r",levels=-3:0.5:3,
+        extend="both"
+    )
+    axs[1].format(
+        title=string(L"$\mu_\Pi$"," ($ID1-$cID)/$cID / %"),coast=true,
+    )
+    axs[1].colorbar(c,loc="r",extend="both")
+
+    c = axs[2].contourf(
+        lon,lat,(ID1_ian.-cID_ian)'*100,
+        cmap="RdBu_r",levels=(-4:4)/2,
+        extend="both"
+    )
+    axs[2].format(
+        title=string(L"$\Delta_a\Pi$"," ($ID1-$cID) / %"),coast=true
+    )
+    axs[2].colorbar(c,loc="r",extend="both",locator=1)
+
+    c = axs[3].contourf(
+        lon,lat,(ID1_dhr.-cID_dhr)'*100,
+        cmap="RdBu_r",levels=-1.5:0.25:1.5,
+        extend="both"
+    )
+    axs[3].format(
+        title=string(L"$\Delta_d\Pi$"," ($ID1-$cID) / %"),coast=true
+    )
+    axs[3].colorbar(c,loc="r",extend="both")
+
+    c = axs[4].contourf(
+        lon,lat,(ID1_sea.-cID_sea)'*100,
+        cmap="RdBu_r",levels=-5:5,
+        extend="both"
+    )
+    axs[4].format(
+        title=string(L"$\Delta_s\Pi$"," ($ID1-$cID) / %"),coast=true
+    )
+    axs[4].colorbar(c,loc="r",extend="both",locator=2)
+
+    c = axs[5].contourf(
+        lon,lat,(ID1_itr.-cID_itr)'*100,
+        cmap="RdBu_r",levels=-5:5,
+        extend="both"
+    )
+    axs[5].format(
+        title=string(L"$\Delta_i\Pi$"," ($ID1-$cID) / %"),coast=true
+    )
+    axs[5].colorbar(c,loc="r",extend="both",locator=2)
+
+    c = axs[6].contourf(
+        lon,lat,((ID2_avg.-cID_avg)./cID_avg)'*100,
+        cmap="RdBu_r",levels=-3:0.5:3,
+        extend="both"
+    )
+    axs[6].format(
+        title=string(L"$\mu_\Pi$"," ($ID2-$cID)/$cID / %"),coast=true,
+    )
+    axs[6].colorbar(c,loc="r",extend="both")
+
+    c = axs[7].contourf(
+        lon,lat,(ID2_ian.-cID_ian)'*100,
+        cmap="RdBu_r",levels=(-4:4)/2,
+        extend="both"
+    )
+    axs[7].format(
+        title=string(L"$\Delta_a\Pi$"," ($ID2-$cID) / %"),coast=true
+    )
+    axs[7].colorbar(c,loc="r",extend="both",locator=1)
+
+    c = axs[8].contourf(
+        lon,lat,(ID2_dhr.-cID_dhr)'*100,
+        cmap="RdBu_r",levels=-1.5:0.25:1.5,
+        extend="both"
+    )
+    axs[8].format(
+        title=string(L"$\Delta_d\Pi$"," ($ID2-$cID) / %"),coast=true
+    )
+    axs[8].colorbar(c,loc="r",extend="both")
+
+    c = axs[9].contourf(
+        lon,lat,(ID2_sea.-cID_sea)'*100,
+        cmap="RdBu_r",levels=-5:5,
+        extend="both"
+    )
+    axs[9].format(
+        title=string(L"$\Delta_s\Pi$"," ($ID2-$cID) / %"),coast=true
+    )
+    axs[9].colorbar(c,loc="r",extend="both",locator=2)
+
+    c = axs[10].contourf(
+        lon,lat,(ID2_itr.-cID_itr)'*100,
+        cmap="RdBu_r",levels=-5:5,
+        extend="both"
+    )
+    axs[10].format(
+        title=string(L"$\Delta_i\Pi$"," ($ID2-$cID) / %"),coast=true
+    )
+    axs[10].colorbar(c,loc="r",extend="both",locator=2)
+
+    for ii = 1 : 10
+        axs[ii].format(abc=true)
+    end
+
+    f.savefig(plotsdir("Pidiff_$(ID1)$(ID2)v$(cID).png"),transparent=false,dpi=200)
+
+end
+
+# plotaxs("REP");
+# plotaxs("REI");
+# plotaxs("RGA"); plotaxs("RGA","REI");
+# plotaxs("EBB"); plotaxs("EBM");
+# plotaxscombined("EBB","EBM");
+#
+# plotaxs2("EG2"); plotaxs2("EG2","REI");
+# plotaxs2("EMN");
+plotaxs2("EMN","REI");
--- scripts/variabilityplot.jl
+++ scripts/variabilityplot.jl
@@ -133,8 +133,85 @@ function plotaxs2(ID::AbstractString)
 
 end
 
-plotaxs("RE5");
-plotaxs("REP"); plotaxs("REI"); plotaxs("RGA")
-plotaxs("EBB"); plotaxs("EBM");
-plotaxs2("EG2");
-plotaxs2("EMN")
+function plottwoaxs(ID1::AbstractString,ID2::AbstractString)
+
+    pplt.close(); proj = pplt.Proj("robin",lon_0=180); f,axs = pplt.subplots(
+        nrows=3,ncols=2,axwidth=3,proj=proj
+    )
+
+    @load datadir("compiled/Pi_$ID1.jld2") lon lat Pi_avg Pi_dhr Pi_sea Pi_ian Pi_itr Pi_var
+    Pi_tot = Pi_dhr + Pi_ian + Pi_sea + Pi_itr + Pi_var
+
+    c = axs[3].contourf(
+        lon,lat,(Pi_ian ./ Pi_tot)'*100,
+        cmap="viridis",levels=0:5:50
+    )
+    axs[3].format(
+        title=L"$\frac{\Delta_a\Pi}{\Delta_t\Pi}$ / $\%$",coast=true,
+    )
+    axs[3].colorbar(c,loc="r")
+
+    c = axs[4].contourf(
+        lon,lat,(Pi_dhr ./ Pi_tot)'*100,
+        cmap="viridis",levels=0:5:50
+    )
+    axs[4].format(
+        title=L"$\frac{\Delta_d\Pi}{\Delta_t\Pi}$ / $\%$",coast=true
+    )
+    axs[4].colorbar(c,loc="r")
+
+    c = axs[5].contourf(
+        lon,lat,(Pi_sea ./ Pi_tot)'*100,
+        cmap="viridis",levels=0:10:100
+    )
+    axs[5].format(
+        title=L"$\frac{\Delta_s\Pi}{\Delta_t\Pi}$ / $\%$",coast=true
+    )
+    axs[5].colorbar(c,loc="r")
+
+    c = axs[6].contourf(
+        lon,lat,(Pi_itr ./ Pi_tot)'*100,
+        cmap="viridis",levels=0:10:100
+    )
+    axs[6].format(
+        title=L"$\frac{\Delta_i\Pi}{\Delta_t\Pi}$ / $\%$",coast=true
+    )
+    axs[6].colorbar(c,loc="r")
+
+    normPi!(Pi_avg,Pi_tot)
+    c = axs[1].contourf(
+        lon,lat,Pi_tot'*100,
+        cmap="viridis",levels=0:2:20,extend="max"
+    )
+    axs[1].format(
+        title=string(L"$\Delta_t\Pi$"," ($ID1) / %"),coast=true,
+    )
+    axs[1].colorbar(c,loc="r",extend="max")
+
+    @load datadir("compiled/Pi_$ID2.jld2") lon lat Pi_avg Pi_dhr Pi_sea Pi_ian Pi_itr Pi_var
+    Pi_tot = Pi_dhr + Pi_ian + Pi_sea + Pi_itr + Pi_var
+
+    normPi!(Pi_avg,Pi_tot)
+    c = axs[2].contourf(
+        lon,lat,Pi_tot'*100,
+        cmap="viridis",levels=0:2:20,extend="max"
+    )
+    axs[2].format(
+        title=string(L"$\Delta_t\Pi$"," ($ID2) / %"),coast=true,
+    )
+    axs[2].colorbar(c,loc="r",extend="max")
+
+    for ii = 1 : 6
+        axs[ii].format(abc=true)
+    end
+
+    f.savefig(plotsdir("Pivar_$ID1$ID2.png"),transparent=false,dpi=200)
+
+end
+
+# plotaxs("RE5");
+# plotaxs("REP"); plotaxs("REI"); plotaxs("RGA")
+# plotaxs("EBB"); plotaxs("EBM");
+plottwoaxs("EBB","EBM")
+# plotaxs2("EG2");
+# plotaxs2("EMN")
