c95fa7c1e1b8d6e379e070349d1953c295634f91
Nathanael Wong <natgeo.wong@outlook.com>
2020-07-09 01:35:32 -0400

    Allow for adjustment of region to analyze

--- src/common.jl
+++ src/common.jl
@@ -20,11 +20,17 @@ function nanrange(data::AbstractArray)
     if dataii != []; return maximum(dataii) - minimum(dataii); else; return NaN; end
 end
 
-function runPiPWV(init::Dict,eroot::Dict,proot::Dict;ID::AbstractString)
+function runPiPWV(
+    init::Dict,eroot::Dict,proot::Dict;
+    ID::AbstractString,regID::AbstractString="GLB"
+)
 
     if ID != "EMN"
 
-        emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="t_mwv_$(ID)");
+        emod,epar,ereg,etime = erainitialize(
+            init,
+            modID="csfc",parID="t_mwv_$(ID)",regID=regID
+        );
 
         if     ID == "RE5";               TmDavisz(emod,epar,ereg,etime,eroot,proot,init);
         elseif any(ID .== ["REP","REI"]); TmDavisp(emod,epar,ereg,etime,eroot,proot,init);
@@ -35,7 +41,7 @@ function runPiPWV(init::Dict,eroot::Dict,proot::Dict;ID::AbstractString)
 
     end
 
-    emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="Pi_$(ID)");
+    emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="Pi_$(ID)",regID=regID);
     if ID != "EMN"
           PiTm(emod,epar,ereg,etime,proot,proot,init);
     else; PiMN(emod,epar,ereg,etime,eroot,proot,init);
@@ -43,35 +49,48 @@ function runPiPWV(init::Dict,eroot::Dict,proot::Dict;ID::AbstractString)
 
 end
 
-function anaPiPWV(init::Dict,eroot::Dict;ID::AbstractString)
+function anaPiPWV(
+    init::Dict,eroot::Dict;
+    ID::AbstractString,regID::AbstractString="GLB"
+)
 
     if ID != "EMN"
 
-        emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="t_mwv_$(ID)");
+        emod,epar,ereg,etime = erainitialize(
+            init,
+            modID="csfc",parID="t_mwv_$(ID)",regID=regID
+        );
         for yr = etime["Begin"] : etime["End"]
             if !((ID == "RGA") && (yr == 1997)); eraanalysis(emod,epar,ereg,yr,eroot); end
         end
 
     end
 
-    emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="Pi_$(ID)");
+    emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="Pi_$(ID)",regID=regID);
     for yr = etime["Begin"] : etime["End"]
         if !((ID == "RGA") && (yr == 1997)); eraanalysis(emod,epar,ereg,yr,eroot); end
     end
 
 end
 
-function compilePiPWV(init::Dict,eroot::Dict;ID::AbstractString)
+function compilePiPWV(
+    init::Dict,eroot::Dict;
+    ID::AbstractString,regID::AbstractString="GLB"
+)
+
 
     if ID != "EMN"
-        emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="t_mwv_$(ID)");
+        emod,epar,ereg,etime = erainitialize(
+            init,
+            modID="csfc",parID="t_mwv_$(ID)",regID=regID
+        );
         lon = ereg["lon"]; lat = ereg["lat"]
         Tm_avg,Tm_var,Tm_dhr,Tm_itr,Tm_sea,Tm_ian = compilePiTm(ID=ID,emod,epar,ereg,etime,eroot)
         if !isdir(datadir("compiled")); mkpath(datadir("compiled")) end
         @save datadir("compiled/Tm_$(ID).jld2") lon lat Tm_avg Tm_var Tm_dhr Tm_itr Tm_sea Tm_ian
     end
 
-    emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="Pi_$(ID)");
+    emod,epar,ereg,etime = erainitialize(init,modID="csfc",parID="Pi_$(ID)",regID=regID);
     lon = ereg["lon"]; lat = ereg["lat"]
     Pi_avg,Pi_var,Pi_dhr,Pi_itr,Pi_sea,Pi_ian = compilePiTm(ID=ID,emod,epar,ereg,etime,eroot)
     if !isdir(datadir("compiled")); mkpath(datadir("compiled")) end
@@ -81,7 +100,7 @@ end
 
 function putinfo(emod::Dict,epar::Dict,ereg::Dict,etime::Dict,eroot::Dict)
 
-    rfol = pwd(); efol = erafolder(emod,epar,ereg,etime,eroot,"sfc");
+    rfol = pwd(); efol = erafolder(emod,epar,ereg,etime,eroot);
     cd(efol["var"]); @save "info_par.jld2" emod epar;
     cd(efol["reg"]); @save "info_reg.jld2" ereg;
     cd(rfol);
